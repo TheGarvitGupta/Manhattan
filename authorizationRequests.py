defc61005a1e3b05e0737d6e37b928d7ba4ad59d
Garvit Gupta <garvit@apple.com>
2020-05-17 00:45:08 -0700

    Some stubbing

--- authorizationRequests.py
+++ authorizationRequests.py
@@ -6,17 +6,10 @@ from globalParams import kStravaClientID
 from urllib import urlencode
 
 def stravaAuthorizeRequest():
-	client_id = kStravaClientID
-	redirect_uri = kStravaRedirectURI
-	response_type = "code"
-	approval_prompt = "force"
-	scope = ",".join(kStravaScope)
-	
 	query_prams_dict = {}
-	query_prams_dict['client_id'] = client_id
-	query_prams_dict['redirect_uri'] = redirect_uri
-	query_prams_dict['response_type'] = response_type
-	query_prams_dict['approval_prompt'] = approval_prompt
-	query_prams_dict['scope'] = scope
-
+	query_prams_dict['client_id'] = kStravaClientID
+	query_prams_dict['redirect_uri'] = kStravaRedirectURI
+	query_prams_dict['response_type'] = "code"
+	query_prams_dict['approval_prompt'] = "force"
+	query_prams_dict['scope'] = ",".join(kStravaScope)
 	return kStravaAuthorizeURL + "?" + urlencode(query_prams_dict)
